8ac18b5974f35a38d60f9ff2b0ea5bb0f577fbd8
Anthony <team.iphone112@gmail.com>
2019-12-04 14:33:46 -0500

    displayed new board and score board

--- scripts.js
+++ scripts.js
@@ -1,4 +1,1 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let scoreBoard = document.getElementById('scoreBoard');
-let resetBoard = document.getElementById('resetBoard');
+  
