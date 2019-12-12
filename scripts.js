98262125db4b1eeda2e5076ead08da57265af141
Melaku <alebachewtegegne2017@yahoo.com>
2019-12-12 11:54:20 -0500

    I selected the id I defined the value of direction snake top and snake left I created keydown events, and controlling the snake usimg arrows to move the snake I added  anew div I created a food using single arroy I created a snake using array of array

--- scripts.js
+++ scripts.js
@@ -1,67 +1,82 @@
-const gameBoard = document.getElementById('gameBoard');
-const snake = document.getElementById('snake');
-const apple = document.getElementById('apple');
-const scoreBoard = document.getElementById('scoreBoard');
-const resetBoard = document.getElementById('resetBoard');
+const scores = document.getElementById("score");
+const end = document.getElementById("gameOver");
+const button = document.getElementById("button");
+// defining
+let direction = "";
+let snakeleft = 100;
+let snaketop = 100;
+let pickedUp = false;
+let score = 0;
 
-//define the units
-let appletop = 200;
-let appleleft = 300;
-let snaketop = 300;
-let snakeleft = 300;
-let value = 300;
-
-// moving the snake
-
-let timer = setInterval(function() {}, 100);
-
+// creating keyevents and controlling the snake using arrow keys
 window.addEventListener("keydown", event => {
-
-  if (event.key == "ArrowLeft") {
-
-      event.preventDefault();
-    clearInterval(timer);
-    timer = setInterval(function() {
-      if (snakeleft >= 0) {
-        snake.style.left = snakeleft + 'px'
-        snakeleft -= 10;
-      }
-    }, 100);
-
-
-  } else if (event.key == "ArrowUp") {
-event.preventDefault();
-    clearInterval(timer);
-
-    timer = setInterval(function() {
-      if (snaketop >= 0) {
-        snake.style.top = snaketop + 'px'
-
-        snaketop -= 10;
-      }
-    }, 100);
-  } else if (event.key == "ArrowRight") {
-event.preventDefault();
-    clearInterval(timer);
-
-    timer = setInterval(function() {
-      if (snakeleft <= 580) {
-        snake.style.left = snakeleft + 'px'
-
-        snakeleft += 10;
-      }
-    }, 100);
-  } else if (event.key == "ArrowDown") {
-event.preventDefault();
-    clearInterval(timer);
-
-    timer = setInterval(function() {
-      if (snaketop <= 580) {
-        snake.style.top = snaketop + 'px'
-
-        snaketop += 10;
-      }
-    }, 100);
+  if (event.key == "ArrowRight") {
+    direction = "right";
   }
-
+  else if (event.key == "ArrowDown") {
+    direction = "down";
+  }
+  else if (event.key == "ArrowUp") {
+    direction = "top";
+  }
+  else if (event.key == "ArrowLeft") {
+    direction = "left";
+  }
+  event.preventDefault();
 });
+const main = document.getElementsByTagName("main")[0];
+let snake = [
+  [snakeleft, snaketop]
+];
+let food = [300, 300];
+let time = setInterval(draw, 100);
+
+
+function draw() {
+
+  while (main.firstChild) {
+    main.firstChild.remove();
+  }
+  if (direction == "right") {
+    snakeleft = snakeleft + 10;
+    snake.unshift([snakeleft, snaketop])
+    if (!pickedUp) {
+      snake.pop();
+    }
+
+  } else if (direction == "down") {
+    snaketop = snaketop + 10;
+    snake.unshift([snakeleft, snaketop]);
+    if (!pickedUp) {
+      snake.pop();
+    }
+  } else if (direction == "left") {
+    snakeleft = snakeleft - 10;
+    snake.unshift([snakeleft, snaketop]);
+    if (!pickedUp) {
+      snake.pop();
+    }
+  } else if (direction == "top") {
+    snaketop = snaketop - 10;
+    snake.unshift([snakeleft, snaketop]);
+    if (!pickedUp) {
+      snake.pop();
+
+    }
+
+  }
+  pickedUp = false;
+  for (const newpart of snake) {
+    const div = document.createElement("div");
+    div.style.left = newpart[0] + "px";
+    div.style.top = newpart[1] + "px";
+    div.style.color = "red";
+    main.appendChild(div);
+  }
+  const apple = document.createElement("div");
+  apple.className = "apple";
+  apple.style.left = food[0] + "px";
+  apple.style.top = food[1] + "px";
+  apple.style.color = "green";
+  main.appendChild(apple);
+}
